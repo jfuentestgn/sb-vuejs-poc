5dbb5ece2f2966144c1ede60ef2302291c1017a7
Juan Fuentes <jfuentestgna@gmail.com>
2020-12-27 10:19:17 +0100

    Initial flyway migrations

---
+++ back/comunidades/src/main/resources/db/migration/V1_2__CreateComunidadTable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `comunidad` (
+	`id` BIGINT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(255) NOT NULL,
+	PRIMARY KEY (`id`)
+)
+COMMENT='Comunidades de propietarios'
+ENGINE=InnoDB
+DEFAULT CHARSET=UTF8MB4
+;
